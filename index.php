7ae01eeb8c60bc3f8f079c7fbdb8f1eadac60d70
aledic85 <dicuollo85@gmail.com>
2019-05-13 15:20:17 +0200

    add class "configurazione"

--- index.php
+++ index.php
@@ -23,6 +23,11 @@
        return $this->idPrenotazioni;
      }
 
+     function getConfigurazioneID() {
+
+       return $this->configurazioneID;
+     }
+
      static function getPrenotazioneByMonth($conn) {
 
       $sql = "SELECT *
@@ -104,14 +109,6 @@
     }
   }
 
-  $conn = new mysqli($servername, $username, $password, $dbname);
-
-    if ($conn->connect_errno) {
-
-      echo ("Connection failed: " . $conn->connect_error);
-      return;
-    }
-
   class Stanza {
 
     private $roomNumber;
@@ -160,18 +157,76 @@
            . "Piano: "
            . $this->floor ."<br>"
            . "Letti: "
-           . $this->beds . "<br>" . "<br>";
+           . $this->beds . "<br>";
     }
   }
+
+  class Configurazione {
+
+    private $title;
+    private $description;
+
+    function __construct($title, $description) {
+
+      $this->title = $title;
+      $this->description = $description;
+    }
+
+    static function getConfigurazioneInfo($conn, $configurazioneID) {
+
+      $sql = "SELECT configurazioni.title, configurazioni.description
+              FROM prenotazioni
+              JOIN configurazioni
+              ON prenotazioni.configurazione_id = configurazioni.id
+              WHERE configurazioni.id = $configurazioneID
+              GROUP BY configurazioni.title";
+
+      $result = $conn->query($sql);
+      $configurazioni = [];
+
+      if ($result->num_rows > 0) {
+
+        while($row = $result->fetch_assoc()) {
+
+          $configurazioni[] = new Configurazione(
+                                $row["title"],
+                                $row["description"]);
+        }
+      } else {
+
+          echo "0 results";
+        }
+
+      return $configurazioni;
+    }
+
+    function printConfigurazioneInfo() {
+
+      echo "Configurazione: "
+            . $this->title . "<br>"
+            . "Descrizione: "
+            . $this->description . "<br>" . "<br>";
+    }
+  }
+
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_errno) {
+
+      echo ("Connection failed: " . $conn->connect_error);
+      return;
+    }
 
   $prenotazioni = Prenotazione::getPrenotazioneByMonth($conn);
 
   foreach ($prenotazioni as $prenotazione) {
 
     $prenotazioneID = $prenotazione->getPrenotazioneId();
+    $configurazioneID = $prenotazione->getConfigurazioneID();
     $printPrenotazione = $prenotazione->printPrenotazioni();
     $ospiti = Ospite::getOspiteById($conn, $prenotazioneID);
     $stanze = Stanza::getStanzeInfo($conn, $prenotazioneID);
+    $configurazioni = Configurazione::getConfigurazioneInfo($conn, $configurazioneID);
 
     foreach ($ospiti as $ospite) {
 
@@ -180,6 +235,11 @@
       foreach ($stanze as $stanza) {
 
         $printStanzaInfo = $stanza->printStanzeInfo();
+      }
+
+      foreach ($configurazioni as $configurazione) {
+
+        $printConfigurazioneInfo = $configurazione->printConfigurazioneInfo();
       }
     }
 
